597ef5b3ac97e3c974592cd58205968a08fe3817
Bruce0203 <BruceFreedy@gmail.com>
2022-06-14 17:58:30 +0900

    add versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ plugins {
     kotlin("plugin.serialization") version "1.6.10"
 }
 
+version = rootProject.properties["version"].toString()
+
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
@@ -50,17 +52,17 @@ allprojects {
 //        testImplementation("org.slf4j:slf4j-api:1.7.36")
 //        testImplementation("org.slf4j:slf4j-simple:1.7.36")
 //        compileOnly(kotlin("test"))
-        implementation("io.github.brucefreedy:mccommand:1.0.3")
-        compileOnly("net.kyori:adventure-api:4.10.1")
+//        implementation("io.github.brucefreedy:mccommand:1.0.3")
 //        compileOnly("io.github.monun:invfx-api:3.1.0")
+        compileOnly("net.kyori:adventure-api:4.10.1")
         compileOnly("com.mojang:authlib:1.5.21")
         compileOnly("org.spigotmc:spigot-api:1.19-R0.1-SNAPSHOT")
-//        compileOnly("org.spigotmc:spigot:1.18.2-R0.1-SNAPSHOT")
-        compileOnly("com.eatthepath:fast-uuid:0.2.0")
+        compileOnly("org.spigotmc:spigot:1.18.2-R0.1-SNAPSHOT")
+//        compileOnly("com.eatthepath:fast-uuid:0.2.0")
 //        compileOnly("net.jafama:jafama:2.3.2")
         compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-        compileOnly("io.github.inggameteam:inggame-modules:1.1.7")
-        compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+//        compileOnly("io.github.inggameteam:inggame-modules:1.1.7")
+//        compileOnly("com.github.MilkBowl:VaultAPI:1.7")
 //        compileOnly("com.rylinaux:PlugMan:2.2.9")
 //        compileOnly("org.mongodb:mongodb-driver-sync:4.6.0")
 //        compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
